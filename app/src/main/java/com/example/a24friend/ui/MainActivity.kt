8457fcfcc91ce61e651759934f6b990f997453e8
mk1990h2 <mikass111@yahoo.co.jp>
2020-01-28 14:07:33 -0800

    add notification

--- app/src/main/java/com/example/a24friend/ui/MainActivity.kt
+++ app/src/main/java/com/example/a24friend/ui/MainActivity.kt
@@ -1,13 +1,85 @@
 package com.example.a24friend.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.a24friend.R
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.InstanceIdResult
+import com.google.firebase.messaging.FirebaseMessaging
+
 
 class MainActivity : AppCompatActivity() {
+
+    // TOPIC must be same as one in the cloud function
+    private val TOPIC = "default"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener(
+            this
+        ) { instanceIdResult: InstanceIdResult ->
+            val newToken = instanceIdResult.token
+            Log.d("newToken", newToken)
+            this.getPreferences(Context.MODE_PRIVATE).edit().putString("fb", newToken)
+                .apply()
+        }
+
+        Log.d(
+            "newToken",
+            this.getPreferences(Context.MODE_PRIVATE).getString("fb", "empty :(")
+        )
+
+        createChannel(
+            applicationContext.getString(R.string.match_notification_channel_id),
+            applicationContext.getString(R.string.match_notification_channel_name))
+
+        subscribeTopic()
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                // change importance
+                NotificationManager.IMPORTANCE_HIGH
+            )
+                // enable badges for this channel
+                .apply {
+                    setShowBadge(true)
+                }
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description =
+                getString(R.string.match_notification_channel_description)
+
+            val notificationManager = this.getSystemService(
+                NotificationManager::class.java
+            )
+
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private fun subscribeTopic() {
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+            .addOnCompleteListener { task ->
+                var message = "Successfully subscribe topic ${TOPIC}."
+                if (!task.isSuccessful) {
+                    message = "failed to subscribe topic ${TOPIC}."
+                }
+                Log.d("subscribeTopic", message)
+            }
     }
 }
