b7d6be86ad2c101a500cbfb0ce202c08b81525fc
Carlos andres Diaz bravo <carlosandresdiazbravo@Carloss-MacBook-Pro.local>
2020-01-30 14:54:34 -0800

    RulesUpdated

--- app/src/main/java/com/example/a24friend/ui/rules/RulesFragment.kt
+++ app/src/main/java/com/example/a24friend/ui/rules/RulesFragment.kt
@@ -1,7 +1,7 @@
 package com.example.a24friend.ui.rules
 
 
-import android.content.res.Configuration
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,15 +10,14 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.a24friend.databinding.FragmentRulesBinding
-
 import com.example.a24friend.R
+
 
 
 /**
  * A simple [Fragment] subclass.
  */
 class RulesFragment : Fragment(){
-
 
 
     override fun onCreateView(
@@ -35,6 +34,7 @@ class RulesFragment : Fragment(){
         binding.rulesNextButton.setOnClickListener { view : View ->
             view.findNavController().navigate(R.id.action_rulesFragment_to_surveyFragment)
         }
+
 
         return binding.root
     }
