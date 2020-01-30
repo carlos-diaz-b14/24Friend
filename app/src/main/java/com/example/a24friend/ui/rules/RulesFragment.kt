607a8e6e17ae1efb07c4f5d2e31141ba683e9fa0
Naoki Mita <naoki.mita18@gmail.com>
2020-01-29 16:47:58 -0800

    Can move from rules to privacy or survey page

--- app/src/main/java/com/example/a24friend/ui/rules/RulesFragment.kt
+++ app/src/main/java/com/example/a24friend/ui/rules/RulesFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.a24friend.databinding.FragmentRulesBinding
 
 import com.example.a24friend.R
 
@@ -18,9 +21,17 @@ class RulesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rules, container, false)
+        val binding = DataBindingUtil.inflate<FragmentRulesBinding>(inflater,
+            R.layout.fragment_rules,container,false)
+
+        binding.rulesPrivacyPolicyMore.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_rulesFragment_to_privacyAgreementFragment)
+        }
+
+        binding.rulesNextButton.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_rulesFragment_to_surveyFragment)
+        }
+
+        return binding.root
     }
-
-
 }
