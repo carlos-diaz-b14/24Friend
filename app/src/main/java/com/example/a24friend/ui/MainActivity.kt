342c70b2dd5f16ec1e415555abfe42f985744756
mk1990h2 <mikass111@yahoo.co.jp>
2020-01-30 15:57:52 -0800

    struggling with Cloud Function

--- app/src/main/java/com/example/a24friend/ui/MainActivity.kt
+++ app/src/main/java/com/example/a24friend/ui/MainActivity.kt
@@ -9,6 +9,8 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.a24friend.R
 import com.example.a24friend.database.getDatabase
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.FirebaseMessaging
 
 
@@ -22,6 +24,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO: this code is to see token
+//        FirebaseInstanceId.getInstance().instanceId
+//            .addOnCompleteListener(OnCompleteListener { task ->
+//                if (!task.isSuccessful) {
+//                    Log.w("MainActivity", "getInstanceId failed", task.exception)
+//                    return@OnCompleteListener
+//                }
+//
+//                // Get new Instance ID token
+//                val token = task.result?.token
+//
+//                // Log and toast
+//                Log.d("MainActivity", "Token: $token")
+//            })
 
         createChannel(
             applicationContext.getString(R.string.match_notification_channel_id),
